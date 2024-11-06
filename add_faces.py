82e7e5e1710aa45a914ec78664e272ff9a65a619
Mehar-Aziz <meharaziz.1614@gmail.com>
2024-11-06 09:24:14 +0500

    open camera

---
+++ add_faces.py
@@ -0,0 +1,13 @@
+import cv2 
+
+video= cv2.VideoCapture(0)
+faces=cv2.CascadeClassifier('/data/haarcascade_frontalface_default.xml')
+while True:
+    ret,frame = video.read()
+    cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Frame", frame)
+    k=cv2.waitKey(1)
+    if k==ord('q'):
+        break
+video.release()
+cv2.destroyAllWindows()
