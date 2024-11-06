313734899f5ce3a443011e6c255ab3aa403f4008
Mehar-Aziz <meharaziz.1614@gmail.com>
2024-11-06 09:49:55 +0500

    Capturing faces data

--- add_faces.py
+++ add_faces.py
@@ -1,13 +1,26 @@
 import cv2 
 
 video= cv2.VideoCapture(0)
-faces=cv2.CascadeClassifier('/data/haarcascade_frontalface_default.xml')
+facedetect=cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
+
+faces_data = []
+i=0
+
 while True:
     ret,frame = video.read()
     gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces=facedetect.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        crop_img=frame[y:y+h, x:x+w, :]
+        resize_img=cv2.resize(crop_img, (50,50))
+        if len(faces_data)<=100 and i%10==0:
+            faces_data.append(resize_img)
+        i=i+1
+        cv2.putText(frame, str(len(faces_data)), (50,50), cv2.FONT_HERSHEY_COMPLEX,1 , (50,50,255), 1)
+        cv2.rectangle(frame, (x,y),(x+w,y+h), (50,50,255), 1)
     cv2.imshow("Frame", frame)
     k=cv2.waitKey(1)
-    if k==ord('q'):
+    if k==ord('q') or len(faces_data) ==100:
         break
 video.release()
 cv2.destroyAllWindows()
