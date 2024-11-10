d657d2d2bf8445d2f3fd761a1f94253f609ecdeb
Mehar-Aziz <meharaziz.1614@gmail.com>
2024-11-10 15:11:59 +0500

    Background fit

--- test.py
+++ test.py
@@ -17,6 +17,8 @@ with open('data/faces_data.pkl', 'rb') as f:
 knn = KNeighborsClassifier(n_neighbors = 5)
 knn.fit(FacesTest, Lables)
 
+imgBackground = cv2.imread('background.png')
+
 while True:
     ret,frame = video.read()
     gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -30,7 +32,8 @@ while True:
         cv2.rectangle(frame, (x,y-40),(x+w,y), (50,50,255), -1)
         cv2.putText(frame, str(output[0]),(x,y-15),cv2.FONT_HERSHEY_COMPLEX,1, (255,255,255), 1)
         cv2.rectangle(frame, (x,y),(x+w,y+h), (50,50,255), 1)
-    cv2.imshow("Frame", frame)
+    imgBackground[175:175 + 480, 115:115 + 640] = frame
+    cv2.imshow("Frame", imgBackground)
     k=cv2.waitKey(1)
     if k==ord('q'):
         break
