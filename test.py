9a3ee9e3deee409bf712e73868a1a7728199155f
Mehar-Aziz <meharaziz.1614@gmail.com>
2024-11-10 16:06:01 +0500

    attendance complete

--- test.py
+++ test.py
@@ -4,6 +4,10 @@ import cv2
 import pickle
 import numpy as np
 import os
+import csv
+import time
+from datetime import datetime
+
 
 video= cv2.VideoCapture(0)
 facedetect=cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
@@ -19,6 +23,8 @@ knn.fit(FacesTest, Lables)
 
 imgBackground = cv2.imread('background.png')
 
+COL_NAMES = ['NAME', 'TIME']
+
 while True:
     ret,frame = video.read()
     gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -27,14 +33,31 @@ while True:
         crop_img=frame[y:y+h, x:x+w, :]
         resize_img=cv2.resize(crop_img, (50,50)).flatten().reshape(1,-1)
         output = knn.predict(resize_img)
+        ts = time.time()
+        date = datetime.fromtimestamp(ts).strftime("%d-%m-%Y")
+        timeStamp = datetime.fromtimestamp(ts).strftime("%H-%M-%S")
+        exist= os.path.isfile('Attendance/Attendance_' + date + '.csv')
         cv2.rectangle(frame, (x,y),(x+w,y+h), (0,0,255), 1)
         cv2.rectangle(frame, (x,y),(x+w,y+h), (50,50,255), 2)
         cv2.rectangle(frame, (x,y-40),(x+w,y), (50,50,255), -1)
         cv2.putText(frame, str(output[0]),(x,y-15),cv2.FONT_HERSHEY_COMPLEX,1, (255,255,255), 1)
         cv2.rectangle(frame, (x,y),(x+w,y+h), (50,50,255), 1)
+        attendance = [str(output[0]), str(timeStamp)]
     imgBackground[175:175 + 480, 115:115 + 640] = frame
     cv2.imshow("Frame", imgBackground)
     k=cv2.waitKey(1)
+    if k==ord('o'):
+        if exist:
+            with open('Attendance/Attendance_' + date + '.csv', '+a') as csvfile:
+                writer =csv.writer(csvfile)
+                writer.writerow(attendance)
+            csvfile.close()
+        else:
+            with open('Attendance/Attendance_' + date + '.csv', '+a') as csvfile:
+                writer =csv.writer(csvfile)
+                writer.writerow(COL_NAMES)
+                writer.writerow(attendance)
+            csvfile.close()
     if k==ord('q'):
         break
 video.release()
